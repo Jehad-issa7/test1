8f663e67841c2c82b832f9badd85c835cf9a2a38
Jehad Issa <88jehiss@skola.boras.se>
2025-11-18 13:32:12 +0100

    first commit

---
+++ Greeter.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace Greeter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        string expected = "Hello, Jehad!";
+
+        //Act 
+        string actual = Program.GetGreeting("Jehad");
+
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Greeter/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    private static void Main()
+    {
+
+    }
+    public static string GetGreeting(string input)
+    {
+        return "Hello, " + input + "!";
+    }
+}
